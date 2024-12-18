bde12b1a26742017c4f73c8a3f47351a9c39f1d0
gnikoloff <nikoloffgeorgi@gmail.com>
2024-12-18 11:15:10 +0100

    add gzip compression

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,30 @@
 import { defineConfig } from "vite";
+import viteCompression from "vite-plugin-compression";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
+	assetsInclude: ["src/assets/**/*"],
 	base: "",
 	build: {
 		outDir: "docs",
+		minify: "terser",
 	},
-	plugins: [topLevelAwait()],
+	plugins: [
+		topLevelAwait(),
+		viteStaticCopy({
+			targets: [
+				{
+					src: "src/assets/sponza/*.{jpg,png,bin}",
+					dest: "assets",
+				},
+			],
+		}),
+		viteCompression(),
+		// mkcert()
+	],
 	server: {
 		host: true,
+		// https: true,
 	},
 });
